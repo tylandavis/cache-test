a68321c572b5820c8d65922370db4a429d2b3644
Tylan Davis <hello@tylandavis.com>
2024-07-12 14:22:05 -0400

    test: trying out DO CDN cache strategy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,33 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import { resolve } from 'path'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  headers: async () => {
+    return [
+      {
+        source: '/(.*?)',
+        headers: [
+          {
+            key: 'CDN-Cache-Control',
+            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+            // value: 's-maxage=0, stale-while-revalidate, stale-if-error',
+          },
+        ],
+      },
+      {
+        source: '/(.*?)',
+        headers: [
+          {
+            key: 'Cloudflare-CDN-Cache-Control',
+            value: 'private, no-cache, no-store, max-age=0, must-revalidate',
+            // value: 's-maxage=0, stale-while-revalidate, stale-if-error',
+          },
+        ],
+      },
+    ]
+  },
+  cacheMaxMemorySize: 0, // disable default in-memory caching
 }
 
 export default withPayload(nextConfig)
